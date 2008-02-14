871029517ca41cc95fc110fe637644b24532ec4f
raja <raja@078453b3-0d46-0410-b6d8-d40f2f97cd17>
2008-02-14 00:23:58 +0000

    l

---
+++ trunk/dev/inc/Util/TypedFunction.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_TYPED_FUNCTION_H
+#define UTIL_TYPED_FUNCTION_H
+
+#include <stdint.h>
+
+#define RIVA_MODULE Util$TypedFunction
+#include <Riva-Header.h>
+
+RIVA_CFUN(void *, get, void *, Std$Type_t *);
+RIVA_CFUN(void, set, void *, Std$Type_t *, void *);
+
+#undef RIVA_MODULE
+
+#endif
