21c4113114312c36adf47d925007eca590bf4f0f
raja <raja@078453b3-0d46-0410-b6d8-d40f2f97cd17>
2008-02-14 00:10:05 +0000

    a

---
+++ trunk/dev/src/Riva/embedded.c
@@ -0,0 +1,26 @@
+#include <gc/gc.h>
+#include "module.h"
+#include "memory.h"
+#include "log.h"
+#include "symbol.h"
+#include "thread.h"
+#include "riva.h"
+#include "native.h"
+#include "directory.h"
+#include "fileset.h"
+
+void init(void) {
+	GC_init();
+	//GC_disable();
+	module_init();
+	memory_init();
+	log_init();
+	config_init();
+	//log_enable();
+	thread_init();
+	directory_init();
+	native_init();
+	fileset_init();
+	riva_init();
+	symbol_init();
+};
